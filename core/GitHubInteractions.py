66d40b4a9536a160b6a988272038d80edff47208
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-12-11 10:40:55 +0900

    Implement GitHub interaction functions

---
+++ core/GitHubInteractions.py
@@ -0,0 +1,40 @@
+import random
+from datetime import datetime
+
+def create_pull_request(repo, branch_name, path, content):
+    pr = repo.create_pull(
+        title=f"{branch_name}",
+        body=(
+            "PR\n\n"
+            f"- {branch_name}\n"
+            f"- {path}\n"
+            "- " + str(datetime.utcnow()) + " UTC\n"
+        ),
+        head=branch_name,
+        base="main"
+    )
+    return pr
+
+def create_direct_commit(repo, path, content, sha):
+    commit = repo.update_file(
+        path,
+        f"Busy update on {path} at {datetime.utcnow()} UTC",
+        content,
+        sha,
+        branch="main"
+    )
+    return commit
+
+def create_issue(repo, issue_title, issue_header, content):
+    issue_body = (
+        f"##{issue_header} \n\n"
+        f"**Date:** {datetime.utcnow()} UTC\n\n"
+        f"> {content}\n\n"
+        "---\n"
+    )
+    issue = repo.create_issue(
+        title=issue_title,
+        body=issue_body,
+        labels=["automation", "mutation"]
+    )
+    return issue
