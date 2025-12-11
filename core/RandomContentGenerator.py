b4aa3861d546e0447abfe5086b01230794847a3e
hotpad100c <46704388+hotpad100c@users.noreply.github.com>
2025-12-11 12:19:29 +0900

    Update RandomContentGenerator.py

--- core/RandomContentGenerator.py
+++ core/RandomContentGenerator.py
@@ -1,5 +1,6 @@
 import requests
 import random
+import string
 
 def generate_random_gibberish(length=50):
     return ''.join(random.choices(string.ascii_letters + string.digits + string.punctuation + ' ', k=length))
